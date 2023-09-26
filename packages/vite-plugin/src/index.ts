b9b91622b7a2974346c6c8679fcb96f701abe9e7
Alex Yang <himself65@outlook.com>
2023-09-26 10:10:53 -0500

    refactor: abstract compile api

--- packages/vite-plugin/src/index.ts
+++ packages/vite-plugin/src/index.ts
@@ -7,6 +7,39 @@ export interface PlugxOptions {
   staticJsonSuffix?: string
 }
 
+interface CompileResult {
+  metadata: {
+    exports: Record<string, string>
+    imports: string[]
+    reexports: Record<string, [string, string][]>
+  }
+  source: string
+}
+
+/**
+ * @internal
+ */
+export function compile (code: string, fileName: string): CompileResult {
+  const record = new StaticModuleRecord(code, fileName)
+  const reexports = record.__reexportMap__ as Record<
+    string,
+    [localName: string, exportedName: string][]
+  >
+  const exports = Object.assign(
+    {},
+    record.__fixedExportMap__,
+    record.__liveExportMap__
+  )
+  return {
+    source: record.__syncModuleProgram__,
+    metadata: {
+      exports: exports,
+      imports: record.imports,
+      reexports: reexports
+    }
+  }
+}
+
 export default function plugx (options: PlugxOptions = {}): import('vite').Plugin {
   const staticJsonSuffix = options.staticJsonSuffix ?? '.static.json'
   return {
@@ -14,31 +47,18 @@ export default function plugx (options: PlugxOptions = {}): import('vite').Plugi
     enforce: 'post',
     renderChunk: function (code, chunk) {
       if (chunk.fileName.endsWith('js')) {
-        const record = new StaticModuleRecord(code, chunk.fileName)
-        const reexports = record.__reexportMap__ as Record<
-          string,
-          [localName: string, exportedName: string][]
-        >
-        const exports = Object.assign(
-          {},
-          record.__fixedExportMap__,
-          record.__liveExportMap__
-        )
+        const { metadata, source } = compile(code, chunk.fileName)
         const fileName = chunk.fileName + staticJsonSuffix
         this.emitFile({
           type: 'asset',
           fileName,
           source: JSON.stringify(
-            {
-              exports: exports,
-              imports: record.imports,
-              reexports: reexports
-            },
+            metadata,
             null,
             2
           )
         })
-        return record.__syncModuleProgram__
+        return source
       }
       return code
     }
