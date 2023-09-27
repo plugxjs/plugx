9b920eb684ea54f880a8f8a2a29be7fbd56aef12
Alex Yang <himself65@outlook.com>
2023-09-27 17:02:03 -0500

    feat(idm): support download from relative url (#13)

--- tests/integration/tests/idm.spec.ts
+++ tests/integration/tests/idm.spec.ts
@@ -7,24 +7,26 @@ let downloader: ReturnType<typeof createDownloader>;
 
 const repository = 'plugxjs/examples';
 
-const mockFetch = fake(async (_input: RequestInfo | URL): Promise<any> => {
-  expect(_input).to.instanceof(URL);
-  const input = _input as URL;
-  const url = input.toString();
-  if (url.endsWith('plugins/basic/package.json')) {
-    return new Response(
-      JSON.stringify({
-        type: 'module',
-        affinePlugin: {
-          release: false,
-          entry: {
-            core: './index.js',
-          },
-        },
-      })
-    );
-  } else if (url.endsWith('plugins/basic/index.js')) {
-    return new Response(`({ imports: $h‍_imports, liveVar: $h‍_live, onceVar: $h‍_once, importMeta: $h‍____meta }) => {
+describe('idm', () => {
+  it('download should work with domain GitHub', async () => {
+    const mockFetch = fake(async (_input: RequestInfo | URL): Promise<any> => {
+      expect(_input).to.instanceof(URL);
+      const input = _input as URL;
+      const url = input.toString();
+      if (url.endsWith('plugins/basic/package.json')) {
+        return new Response(
+          JSON.stringify({
+            type: 'module',
+            affinePlugin: {
+              release: false,
+              entry: {
+                core: './index.js',
+              },
+            },
+          })
+        );
+      } else if (url.endsWith('plugins/basic/index.js')) {
+        return new Response(`({ imports: $h‍_imports, liveVar: $h‍_live, onceVar: $h‍_once, importMeta: $h‍____meta }) => {
   let c;
   $h‍_imports([["./utils.js", []], ["not-exist-module", [["c", [($h‍_a) => c = $h‍_a]]]]]);
   Object.defineProperty(d, "name", { value: "d" });
@@ -37,8 +39,8 @@ const mockFetch = fake(async (_input: RequestInfo | URL): Promise<any> => {
   }
 };
 `);
-  } else if (url.endsWith('plugins/basic/utils.js')) {
-    return new Response(`({ imports: $h‍_imports, liveVar: $h‍_live, onceVar: $h‍_once, importMeta: $h‍____meta }) => {
+      } else if (url.endsWith('plugins/basic/utils.js')) {
+        return new Response(`({ imports: $h‍_imports, liveVar: $h‍_live, onceVar: $h‍_once, importMeta: $h‍____meta }) => {
   $h‍_imports([]);
   function test() {
   }
@@ -47,8 +49,8 @@ const mockFetch = fake(async (_input: RequestInfo | URL): Promise<any> => {
   }
 };
 `);
-  } else if (url.endsWith('plugins/basic/index.js.static.json')) {
-    return new Response(`{
+      } else if (url.endsWith('plugins/basic/index.js.static.json')) {
+        return new Response(`{
   "exports": {
     "b": [
       "b"
@@ -66,33 +68,66 @@ const mockFetch = fake(async (_input: RequestInfo | URL): Promise<any> => {
   ],
   "reexports": {}
 }`);
-  } else if (url.endsWith('plugins/basic/utils.js.static.json')) {
-    return new Response(`{
+      } else if (url.endsWith('plugins/basic/utils.js.static.json')) {
+        return new Response(`{
   "exports": {},
   "imports": [],
   "reexports": {}
 }`);
-  } else {
-    throw new Error(`Unexpected URL: ${url}`);
-  }
-});
-
-beforeEach(() => {
-  downloader = createDownloader({
-    domain: Domain.GitHub,
-    network: {
-      fetch: mockFetch,
-    },
-    packageSection: 'affinePlugin',
-  });
-});
-
-describe('idm', () => {
-  it('download should work', async () => {
+      } else {
+        throw new Error(`Unexpected URL: ${url}`);
+      }
+    });
+    downloader = createDownloader({
+      domain: Domain.GitHub,
+      network: {
+        fetch: mockFetch,
+      },
+      packageSection: 'affinePlugin',
+    });
     const { js, entry } = await downloader.download(repository, './plugins/basic/package.json');
     const sandbox = createPlugxSandbox();
     const fn = sandbox.evaluate(entry.core);
     expect(fn).to.instanceof(Function);
     expect(js.has('./utils.js')).to.be.true;
+  });
+
+  it('download should work with domain URL', async () => {
+    const mockFetch = fake(async (_input: RequestInfo | URL): Promise<any> => {
+      const input = _input as URL;
+      const url = input.toString();
+      if (url === `http://localhost:8080/relative/${repository}/package.json`) {
+        return new Response(
+          JSON.stringify({
+            type: 'module',
+            affinePlugin: {
+              release: false,
+              entry: {
+                core: './index.js',
+              },
+            },
+          })
+        );
+      } else if (url === `http://localhost:8080/relative/${repository}/index.js`) {
+        return new Response('console.log("hello world");');
+      } else if (url === `http://localhost:8080/relative/${repository}/index.js.static.json`) {
+        return new Response(`{
+  "exports": {},
+  "imports": [],
+  "reexports": {}}`);
+      } else {
+        throw new Error(`Unexpected URL: ${url}`);
+      }
+    });
+    const downloader = createDownloader({
+      domain: new URL('http://localhost:8080/relative/'),
+      network: {
+        fetch: mockFetch,
+      },
+      packageSection: 'affinePlugin',
+    });
+    const { js, entry } = await downloader.download(repository);
+    expect(entry.core).to.be.eq('console.log("hello world");');
+    expect(js.size).to.be.eq(0);
   });
 });
